1eddd540f77e9551d8ee4d9b9060b98b287b43c7
lininglouis <lininglouis@hotmail.com>
2019-04-12 23:39:27 +0000

    aa

--- run_test.sh
+++ run_test.sh
@@ -6,5 +6,5 @@ EPOCH=20
 CHECKPOINT=796
 
 python test_net.py --dataset colony --net res101\
-    --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT \
-    --cuda
+    --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT 
+    #--cuda
